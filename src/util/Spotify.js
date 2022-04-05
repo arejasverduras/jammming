eeeb2331203cf29f6a5ffafcac3f3a8ef052d01a
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-04-05 16:18:21 +0200

    request access token spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,6 @@
+const clientID = 'df3bf367945c41c49b4f5e1d9de975b3';
+const redirectURI = "http://localhost:3000/";
+
 let userAccessToken;
 
 const Spotify = {
@@ -5,7 +8,41 @@ const Spotify = {
         if (userAccessToken) {
             return userAccessToken;
         }
-    } 
+//      check if response url contains accesstoken
+        let urlString = window.location.href;
+
+        const accesTokenMatch = urlString.match('/access_token=([^&]*)/');
+        const expiresInMatch =  urlString.match('/expires_in=([^&]*)/')
+        
+        if (accesTokenMatch && expiresInMatch)       
+            {
+                userAccessToken = accesTokenMatch[1];
+                const expiresIn = Number(expiresInMatch[1]);
+                
+                window.setTimeout(() => userAccessToken = '', expiresIn * 1000);
+                window.history.pushState('Access Token', null, '/');
+                return userAccessToken;
+            } else {
+                const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+                window.location = accessUrl;
+            }
+            
+    },
+    search(searchTerm) {
+        //accepts search term input paramter
+        //pass the search term value (term) to a Spotify Request
+        fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Request failed!');
+        }, networkError => console.log(networkError.message)
+        ).then(jsonResponse => {
+            //code to execute with jsonReeponse
+            // return the response as a list of tracks in json format
+
+        })    
+    }  
 };
 
 export default Spotify;
