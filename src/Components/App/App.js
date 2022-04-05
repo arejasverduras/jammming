0e7e74c3c9945e1a398618b3e7c85f743f505fcb
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-04-05 12:35:19 +0200

    write savePlaylist method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -38,6 +38,7 @@ export class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
     
   }
 
@@ -64,6 +65,11 @@ export class App extends React.Component {
       this.setState({playlistName: name})
     }
 
+    savePlaylist() {
+      const trackURIs = this.state.playlistTracks.map(track => 
+        track.uri); 
+    }
+
   render() {
     return (
     <div>
@@ -79,6 +85,7 @@ export class App extends React.Component {
         playlistTracks={this.state.playlistTracks}
         onRemove={this.removeTrack}
         onNameChange={this.updatePlaylistName}
+        onSave={this.savePlaylist}
         />
       </div>
     </div>
