9cf7801b9e0a1c9287484f9dbda06d4b69fc1488
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-04-06 14:57:47 +0200

    update savePlaylist method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -72,6 +72,7 @@ export class App extends React.Component {
     savePlaylist() {
       const trackURIs = this.state.playlistTracks.map(track => 
         track.uri); 
+        Spotify.savePlaylist(this.state.playlistName, trackURIs);
       
     }
 
